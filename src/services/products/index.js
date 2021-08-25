d7effc76484f3a5bbbd574c146b9b5b3b1d4d36e
Luna Sarmiento <selene942008@hotmail.com>
2021-08-25 17:36:34 +0200

    Update index.js

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -24,8 +24,6 @@ router
 
       console.log({ name: `%${name}` });
       const data = await Product.findAll({
-        //  attributes: ["id", "name"],   select columns
-        attributes: { exclude: ["image_url"] }, // exclude columns
         ...filter,
         include: Review,
       });
